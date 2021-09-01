5a01d1b30ce156546e218ccd779af99b0979ee5b
Ismaïl Lachheb <ismael.lachheb@protonmail.com>
2021-09-01 19:27:36 +0200

    wip

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	$:tasks = [];
+	let addtask = '';
+
+	function addTask(){
+		let newtask = {
+			//'id': tasks.length +1,
+			'status': 'Unstarted',
+			'value': addtask
+		}
+		tasks = [...tasks, newtask]
+		console.log(tasks);
+	}
+
+
+</script>
+
+<main>
+	<h1>Best TODO APP for Skooleurs ! </h1>
+
+	<div>
+		<input bind:value={addtask} type="text" id="addtask">
+		<button on:click={addTask}>add a task</button>
+	</div>
+
+	<div>
+		{#each tasks as task, i}
+			<li>
+				{task.value}
+			</li>
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
